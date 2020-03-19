c86c8e004f8a1588c308d0a0e8bbfa211d587771
Jurmmander <52196622+Jurmmander@users.noreply.github.com>
2020-03-19 18:06:03 +0300

    add

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of argumens!");
+                Console.WriteLine("Right form: remove_duplicates <input string>");
+                return;
+            }
+            HashSet<char> charset = new HashSet<char>();
+            String str = args[0];
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(!charset.Contains(str[i]))
+                {
+                    charset.Add(str[i]);
+                }
+                else
+                {
+                    str = str.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
