d48e78d0d249be4f5135cebd3f4047686d6ad35e
Jurmmander <52196622+Jurmmander@users.noreply.github.com>
2020-04-04 14:15:36 +0300

    fixed

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of argumens!");
+                Console.WriteLine("Right form: remove_duplicates <input string>");
+                return;
+            }
+            String str = args[0];
+            str = new string(str.Distinct().ToArray());
+            Console.WriteLine(str);
+        }
+    }
+}
